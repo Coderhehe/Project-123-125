2fa3d4cf30a4efb9445383fba77d79864d3868d6
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-07-07 22:33:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,13 +10,21 @@ posenet.on('pose',gotResult);
 function modelLoaded(){
     console.log("PoseNet is Initialized");
 }
-
+leftw=0;
+rightw=0;
+differnce=0;
 function draw() {
     background(51);
+    textSize(differnce);
+    fill(60,150,120);
+    text("Font Size Is Here",50,400);
 }
 
 function gotResult(result) {
     if(result.length > 0){
  console.log(result);
+ leftw=result[0].pose.leftWrist.x;
+ rightw=result[0].pose.rightWrist.x;
+ differnce=floor(leftw-rightw);
     }
 }
